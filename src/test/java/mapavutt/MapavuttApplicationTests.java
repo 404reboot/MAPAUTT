672f7ba91f94761e4fe6a8f5c6da862dadfd96f9
dexslender <dexslender@proton.me>
2026-06-20 22:08:51 -0600

    feat: inicializar la estructura del proyecto con Spring Boot y Maven.

---
+++ src/test/java/mapavutt/MapavuttApplicationTests.java
@@ -0,0 +1,13 @@
+package mapavutt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapavuttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
