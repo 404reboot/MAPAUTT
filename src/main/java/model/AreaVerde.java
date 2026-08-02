ef4249778a5fc63473eae41ec1eebcc5afc99978
Eduardo J. <dexslender@proton.me>
2026-08-02 02:05:32 -0600

    enable jpa models and define database tables * implement administrator repository * add model annotations * enable model scan in Main

--- src/main/java/model/AreaVerde.java
+++ src/main/java/model/AreaVerde.java
@@ -1,6 +1,16 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "area_verde")
 public class AreaVerde {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private String sector;
--- src/main/java/model/Edificio.java
+++ src/main/java/model/Edificio.java
@@ -1,6 +1,16 @@
 package model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "edificio")
 public class Edificio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String carreras;
