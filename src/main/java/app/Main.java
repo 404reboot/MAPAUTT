929b633dcc29f85f472a56d3a58333e1917fbeb3
Eduar J. <eduarzamora340@gmail.com>
2026-08-12 09:25:07 -0600

    Revert "Merge into main (#16)"
    This reverts commit 726d8a3e4104464684c334b19f4ca5afcb9ea266.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,12 +2,8 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"app", "controller", "model"})
-@EnableJpaRepositories(basePackages = "model")
-@EntityScan(basePackages = "model")
+@SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
@@ -15,4 +11,3 @@ public class Main {
 	}
 
 }
-
